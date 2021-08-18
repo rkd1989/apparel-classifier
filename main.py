7c5c68eeec40f18b75dcc9760d86bcf190e16a4d
Rishabh Kavidayal <34520858+rkd1989@users.noreply.github.com>
2021-08-18 15:44:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -142,3 +142,8 @@ print(img.shape)
 predictions_single = probability_model.predict(img)
 
 print(predictions_single)
+
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
